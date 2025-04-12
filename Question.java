54d51d0589594e7462959de83b29301f60d77e05
Isbaric <luis.mv79cs@gmail.com>
2025-04-12 18:35:58 +0000

    Worked on the questions themselves and added more to the Question class.

--- Question.java
+++ Question.java
@@ -2,15 +2,27 @@ public class question {
     String text;
     String[] choices;
     int correctAnswer;
+    String reason;
 
-    public Question(String text, String[] choices, int correctAnswer)
+    public Question(String text, String[] choices, int correctAnswer, String reason)
     {
         this.text = text;
         this.choices = choices;
         this.correctAnswer = correctAnswer;
+        // I added this so if the answer is wrong the user can get an explanation on why its wrong.
+        this.reason = reason;
     }
 
     public boolean checkAnswer(int userAnswer) {
-        
+        return userAnwer == correctAnwer;
     }
+
+    public String() {
+        return text + " " + choices;
+    }
+
+    public reasonString() {
+        return reason;
+    }
+
 }
--- QuizGame.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-
-public class QuizGame
-{
-/*
-    ArrayList questions = ;
-*/
-}
